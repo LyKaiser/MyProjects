7a8c560fd2ba3c84313a4befe7b0fe53e3e27bba
Lydia <ly.kaiser@web.de>
2020-11-18 15:31:59 +0100

    HWPr gelöscht

--- HardwarePraktikum/section_4/test1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import os
-import time
-import datetime
